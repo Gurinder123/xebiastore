98ecf3036dfe081b45cd751bc86c119b57e23c25
Gurinder123 <gsingh.2090@gmail.com>
2015-04-26 19:34:38 +0530

    modification in login controller and test cases added userstatus model

---
+++ src/main/java/hello/model/UserStatus.java
@@ -0,0 +1,25 @@
+package hello.model;
+
+/**
+ * Created by gurinder on 26/4/15.
+ */
+public class UserStatus {
+
+    private String status;
+
+    public UserStatus() {
+
+    }
+
+    public UserStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
